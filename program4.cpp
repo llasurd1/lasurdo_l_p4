874048563d37c61ee84c8bc07203bbf0bfc1ea72
Liam Lasurdo <38768478+llasurd1@users.noreply.github.com>
2020-11-24 12:01:51 -0500

    Update program4.cpp

--- program4.cpp
+++ program4.cpp
@@ -97,12 +97,12 @@ int main(int argc, char *argv[]) {
 			}
 		}*/
 		cout << items[u] << " " << mp << " " <<endl;
-		/*for(int i = 0; i<n; i++) {
+		for(int i = 0; i<n; i++) {
 			for(int j = 0; j<cap; j++) {
-				cout << darray[i][j] << " ";	
+				darray[i][j] = 0;	
 			}
 			cout << endl;
-		}*/
+		}
 			
 	}
 	
